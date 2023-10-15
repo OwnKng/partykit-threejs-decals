46b2f2463c0f9996734eb2ff553c5098448864e7
OwnKng <owen.king@outlook.com>
2023-10-15 18:35:49 +0100

    wip

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,107 @@
-import type * as Party from "partykit/server";
+import type * as Party from "partykit/server"
+
+type Cursor = {
+  id: string
+  uri: string
+  x?: number
+  y?: number
+  pointer?: "mouse" | "touch"
+  lastUpdate?: number
+}
+
+type ConnectionWithCursor = Party.Connection & { cursor?: Cursor }
 
 export default class Server implements Party.Server {
   constructor(readonly party: Party.Party) {}
 
   onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.party.id}
-  url: ${new URL(ctx.request.url).pathname}`
-    );
+    let cursors = {}
+
+    for (const ws of this.party.getConnections()) {
+      const id = ws.id
+
+      let cursor =
+        (ws as ConnectionWithCursor).cursor || ws.deserializeAttachment()
+
+      if (
+        id !== conn.id &&
+        cursor !== null &&
+        cursor.x !== undefined &&
+        cursor.y !== undefined
+      ) {
+        cursors[id] = cursor
+      }
+    }
+
+    const message = {
+      type: "sync",
+      cursors,
+    }
 
     // let's send a message to the connection
-    conn.send("hello from server");
+    conn.send(JSON.stringify(message))
   }
 
   onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.party.broadcast(
-      `${sender.id}: ${message}`,
-      // ...except for the connection it came from
-      [sender.id]
-    );
+    const position = JSON.parse(message)
+
+    const cursor = {
+      id: sender.id,
+      x: position.x,
+      y: position.y,
+      uri: position.uri,
+      pointer: position.pointer,
+      lastUpdate: Date.now(),
+    }
+
+    this.setCursor(sender as ConnectionWithCursor, cursor)
+
+    const msg =
+      position.x && position.y
+        ? {
+            type: "update",
+            ...cursor,
+            id: sender.id,
+          }
+        : {
+            type: "remove",
+            id: sender.id,
+          }
+
+    this.party.broadcast(JSON.stringify(msg), [sender.id])
+  }
+
+  getCursor(connection: ConnectionWithCursor) {
+    if (!connection.cursor) {
+      connection.cursor = connection.deserializeAttachment()
+    }
+
+    return connection.cursor
+  }
+
+  setCursor(connection: ConnectionWithCursor, cursor: Cursor) {
+    const previousCursor = connection.cursor
+    connection.cursor = cursor
+
+    if (
+      !previousCursor ||
+      !previousCursor.lastUpdate ||
+      (cursor.lastUpdate && cursor.lastUpdate - previousCursor.lastUpdate > 100)
+    ) {
+      connection.serializeAttachment({
+        ...cursor,
+      })
+    }
+  }
+
+  onClose(connection: Party.Connection<unknown>): void | Promise<void> {
+    const message = {
+      type: "remove",
+      id: connection.id,
+    }
+
+    this.party.broadcast(JSON.stringify(message))
   }
 }
 
-Server satisfies Party.Worker;
+Server satisfies Party.Worker
